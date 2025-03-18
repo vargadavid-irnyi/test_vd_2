c0c73fe0d9183df4d5438d9c641c71c78838f2cd
vargad <vargadavid@irinyi-ref.hu>
2025-03-18 14:04:57 +0100

    Fejlesztés input

--- git2.py
+++ git2.py
@@ -2,6 +2,8 @@ print('Hello py git! :)')
 a = 5
 b = 10
 print(a+b)
-
+a = int(input('Kérek egy egész számot: '))
+b = int(input('Kérek egy másik egész számot: '))
+print(f"Összegük: {a+b}")
 for i in range(1,4):
     print(f"Még {i} hét!")
